0dc5962e98062a047e7dc5762e10a52f24c7ed42
Tung Zie Yeong <ohqoo123@gmail.com>
2019-09-23 18:30:20 +0800

    Added player item pick up and pick up notification, but the counter couldnt work properly

---
+++ Back_Home/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Vector3 direction = Vector3.zero;
+    public float speed;
+
+    private void Update()
+    {
+        direction = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.D))
+            direction.x += speed;
+        else if (Input.GetKeyDown(KeyCode.A))
+            direction.x -= speed;
+        else if (Input.GetKeyDown(KeyCode.W))
+            direction.y += speed;
+        else if (Input.GetKeyDown(KeyCode.S))
+            direction.y -= speed;
+
+        transform.Translate(direction);
+    }
+}
---
+++ Back_Home/Assets/Scripts/PlayerPickUpCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPickUpCounter : MonoBehaviour
+{
+    [SerializeField] private Text iceCounterText; 
+    [SerializeField] private Text titaniumCounterText; 
+
+    private int iceAmount; // increment of Ice
+    private int titaniumAmount; // increment of titanium
+
+    private void Update()
+    {
+        iceCounterText.text = "ICE : " + iceAmount.ToString();
+        titaniumCounterText.text = "TITANIUM : " + titaniumAmount.ToString();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if ((Input.GetKeyUp(KeyCode.K)) && collision.GetComponent<IcePickUp>()) // i couldnt get the counter work properly even 
+        {
+            iceAmount += 1;                                                     // if i use without input keycode, could work but collide will 
+                                                                                //  automatically increase the counter
+        }
+        if ((Input.GetKeyUp(KeyCode.K)) && collision.GetComponent<TitaniumPickUp>())
+        {
+            titaniumAmount += 1;
+        }
+    }
+}
