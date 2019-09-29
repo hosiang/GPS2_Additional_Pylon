a0ae5de12be1bad2a5f140fa7113d872aec8fb2c
hosiang <hosiang79@hotmail.com>
2019-09-29 08:37:51 +0800

    Update: -Create a ShipEntity class

---
+++ Back_Home/Assets/Scripts/ShipEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipEntity : MonoBehaviour
+{
+    private float healthPoint = 0;
+    private float healthPointMaximal = 0;
+
+    private float weightAmount = 0;
+    private float weightAmountMaximal = 0;
+
+    private float energyPoint = 0; // nitroPoint?
+    private float energyPointMaximal = 0;
+    private float energyDepletePoint = 0;
+
+    //private float 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
