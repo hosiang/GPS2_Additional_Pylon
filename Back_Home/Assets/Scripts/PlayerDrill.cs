ae5ea19f0e94b4a2ef1cbb82acfaa640d72c8c94
Tung Zie Yeong <ohqoo123@gmail.com>
2019-10-02 23:09:07 +0800

    Added asteroid destroy, asteroid bounce, ores scattered, ores picked up

---
+++ Back_Home/Assets/Scripts/PlayerDrill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDrill : MonoBehaviour
+{
+    private bool isDestroyed = false;
+
+    [SerializeField] private GameObject ores;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Asteroid")
+        {
+            Destroy(other.gameObject);
+            isDestroyed = true;
+            if (isDestroyed)
+            {
+                for (int i = 0; i < Random.Range(1,5); i++)
+                {
+                    Vector3 pos = other.transform.position + new Vector3(Random.Range(1, 5), Random.Range(0, 1), Random.Range(0, 1));
+                    GameObject go = Instantiate(ores, pos, Quaternion.identity);
+                }
+                isDestroyed = false;
+            }
+        }
+    }
+}
