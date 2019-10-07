1746df8179a382c3ba690dbe04160b3a541efbd8
Kenze Kua <kenzekua@gmail.com>
2019-10-07 20:36:52 +0800

    Made menu system, added Main Menu test and Pause Menu test. Made Parallax script [haven't tested]

---
+++ Back_Home/Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    private bool InGame = false;
+    private bool IsPause = true;
+
+    [SerializeField] GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPause();
+    }
+
+    void CheckPause()
+    {
+        if (InGame)
+        {
+            if (!IsPause && Input.GetKeyDown(KeyCode.Escape))
+            {
+                PauseGame();
+            }
+            else if (IsPause && Input.GetKeyDown(KeyCode.Escape))
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void NewGame()
+    {
+        Time.timeScale = 1f;
+        InGame = true;
+        IsPause = false;
+        //SceneManager.LoadScene("Kenze");
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1f;
+        InGame = true;
+        IsPause = false;
+        //SceneManager.LoadScene("Back Home");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log($"<color=red>Quit</color>");
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        IsPause = true;
+        pauseMenu.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        IsPause = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void SetInGame(bool value)
+    {
+        InGame = value;
+    }
+}
---
+++ Back_Home/Assets/Scripts/Parallax.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] float parallaxEffect;
+    private float length;
+    private float startPosition;
+    [SerializeField] GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.z;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float temporary = (camera.transform.position.x * (1 - parallaxEffect));
+        float distance = (camera.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if (temporary > startPosition + length)
+        {
+            startPosition += length;
+        }
+        else if (temporary < startPosition - length)
+        {
+            startPosition -= length;
+        }
+    }
+}
