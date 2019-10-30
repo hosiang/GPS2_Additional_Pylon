552dd0f060d007f6262f3982374b8f1665a1adc3
Tung Zie Yeong <ohqoo123@gmail.com>
2019-10-30 22:22:31 +0800

    drill timer

---
+++ Back_Home/Assets/Scripts/Ores.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ores : MonoBehaviour
+{
+    [SerializeField] private Global.OresTypes oresType;
+    private float[] AstroidOreProvide = { 3, 5, 4 };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PickUpOre();
+        }
+    }
+    private void PickUpOre()
+    {
+        switch (oresType)
+        {
+            case Global.OresTypes.Iron:
+                FindObjectOfType<ShipEntity>().GainOresFromAsteroid(this, Global.OresTypes.Iron, AstroidOreProvide[(int)Global.AstroidType.small]);
+                break;
+            case Global.OresTypes.no2_Ores:
+                FindObjectOfType<ShipEntity>().GainOresFromAsteroid(this, Global.OresTypes.Iron, AstroidOreProvide[(int)Global.AstroidType.big]);
+                break;
+            case Global.OresTypes.Length:
+                FindObjectOfType<ShipEntity>().GainOresFromAsteroid(this, Global.OresTypes.no2_Ores, AstroidOreProvide[(int)Global.AstroidType.special]);
+                break;
+        }
+        Destroy(gameObject);
+        
+    }
+}
