79c0a985b84764c500f9ec639b324ef74f88b7c2
Kenze Kua <kenzekua@gmail.com>
2019-11-02 23:28:15 +0800

    - CameraZoom out script - Parallax script

---
+++ Back_Home/Assets/Scripts/Systems/CameraZoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+
+    [SerializeField] float startSize = 2f;
+    [SerializeField] float endSize = 10f;
+    [SerializeField] float sizeRate = 0.1f;
+    private bool isZooming = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.orthographicSize = startSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainCamera.orthographicSize >= 10f)
+        {
+            isZooming = false;
+            mainCamera.orthographicSize = endSize;
+            StopCoroutine(Zoom());
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Zoom());
+        }
+    }
+
+    private IEnumerator Zoom()
+    {
+        while(isZooming)
+        {
+            mainCamera.orthographicSize += sizeRate;
+            yield return new WaitForSeconds(0.0001f);
+        }
+    }
+}
